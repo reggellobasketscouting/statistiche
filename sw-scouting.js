bbb99dc275d6300d6b9b5bf3682fbf2e514e2cf7
reggellobasketscouting <caini.alessandro@libero.it>
2026-01-25 14:29:36 +0100

    Add files via upload

---
+++ sw-scouting.js
@@ -0,0 +1,42 @@
+const CACHE = "basket-scouting-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest-scouting.webmanifest",
+  "./sw-scouting.js",
+  "./icons/scouting-192.png",
+  "./icons/scouting-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  // Cache-first only for same-origin
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(req).then(cached => {
+        if (cached) return cached;
+        return fetch(req).then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(req, copy));
+          return res;
+        });
+      })
+    );
+  }
+});
